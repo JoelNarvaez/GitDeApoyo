24fbf0e531e69436ee1b74f7d483d13472ea996b
JoelNarvaez <joelnarmar20@gmail.com>
2024-11-29 17:55:40 -0600

    Mensaje de bienvenida

--- Portada.cs
+++ Portada.cs
@@ -135,7 +135,7 @@ namespace WinFormsPortada2
 
         private void pictureBox37_Click(object sender, EventArgs e)
         {
-          
+            MessageBox.Show("Bienveinido");
         }
     }
 }
--- obj/Debug/net6.0-windows/WinFormsPortada2.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/WinFormsPortada2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb512b345e6d63bd1bc303d3c75c23ab250448b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dfd9942993cf4cf3203b780789bc1402d5de4fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
