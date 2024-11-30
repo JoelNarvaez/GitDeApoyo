516cce1e080f5e695d82b60189a6f5ebbbcd1677
JoelNarvaez <joelnarmar20@gmail.com>
2024-11-30 17:35:44 -0600

    Ajustando los repositorios

--- obj/Debug/net6.0-windows/WinFormsPortada2.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/WinFormsPortada2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dfd9942993cf4cf3203b780789bc1402d5de4fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00ed77039d8ada074f5eb6a207c3a3d9cfa61777")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
