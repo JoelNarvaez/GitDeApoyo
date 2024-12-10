505e64de91c5bd8c349548300330c368f888f465
JoelNarvaez <joelnarmar20@gmail.com>
2024-12-10 10:13:40 -0600

    oki

--- WinFormsPortadaJ/obj/Debug/net8.0-windows/WinFormsPortada2.AssemblyInfo.cs
+++ WinFormsPortadaJ/obj/Debug/net8.0-windows/WinFormsPortada2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6607a23107c60ef69115ed3931be3c0d9f27d48d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+526015c1968c406de84148dcadd0d01fc5d916c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinFormsPortada2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
