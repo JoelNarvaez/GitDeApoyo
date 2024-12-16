a92ccd1ad1a0a14843b7eb4da021c43223daf889
JoelNarvaez <joelnarmar20@gmail.com>
2024-12-15 18:30:21 -0600

    grafica

--- UsuarioYAdmin/projectM/projectM/projectM/Grafica.Designer.cs
+++ UsuarioYAdmin/projectM/projectM/projectM/Grafica.Designer.cs
@@ -28,29 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             label1 = new Label();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Century Gothic", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(197, 107);
+            label1.Font = new Font("Century Gothic", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.BlueViolet;
+            label1.Location = new Point(358, 9);
             label1.Name = "label1";
-            label1.Size = new Size(134, 23);
+            label1.Size = new Size(439, 28);
             label1.TabIndex = 0;
-            label1.Text = "GRAFICAS......";
+            label1.Text = "GRAFICA de relacion  Usuario Monto";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(26, 53);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(1150, 585);
+            chart1.TabIndex = 1;
+            chart1.Text = "chart1";
             // 
             // Grafica
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1200, 650);
+            Controls.Add(chart1);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Grafica";
             Text = "Grafica";
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -58,5 +82,6 @@
         #endregion
 
         private Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
